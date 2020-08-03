19021e3577a4b7b3128b982c56666b293823858a
laylatul maghfiroh <67495310+laylatulmaghfiroh@users.noreply.github.com>
2020-08-03 17:21:36 +0700

    Add files via upload

---
+++ src/tugaske4/Array1Dimensi.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author User
+ */
+public class Array1Dimensi {
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String kota[] = {"Mojokerto","Bali","Yogyakarta","Blitar"};
+        
+        System.out.println("cek kota : "+kota[0]);
+        
+    }
+    
+ 
+}
